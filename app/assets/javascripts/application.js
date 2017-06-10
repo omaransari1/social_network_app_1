5f093381c5f2205429dd57d31c5d91e1336025ce
Omar Ansari <omaransari1@gmail.com>
2017-06-10 15:55:52 -0500

    Add plugins to vendor folder: fullcalendar, moment, bindwithdelay

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,16 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.atwho
 //= require jquery_ujs
 //= require bootstrap
 //= require bindWithDelay
+//= require moment.min
+//= require fullcalendar.min
 //= require jquery.datetimepicker
 //= require_tree .
+
+$(document).ready(function() {
+  $("#event_event_datetime").datetimepicker({format: 'Y/m/d H:i'});
+  $("#user_dob").datetimepicker({timepicker: false, format: 'Y/m/d', maxDate: '0'});
+});
