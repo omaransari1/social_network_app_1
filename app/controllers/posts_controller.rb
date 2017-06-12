3feec23bea065c2e0c69a68ce525a04810ea7d58
Omar Ansari <omaransari1@gmail.com>
2017-06-11 19:32:34 -0500

    Generated uploader, Comment model, and Vote model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
+
     if @post.save
       redirect_to @post, notice: 'Post was successfully created.'
     else
