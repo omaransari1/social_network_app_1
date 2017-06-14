7575293ea219ced236f5d72a017e71e50c9a4aa6
Omar Ansari <omaransari1@gmail.com>
2017-06-14 03:41:19 -0500

    Created migration for acts_as_follower and follow model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  acts_as_follower
+  acts_as_followable
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
