d316e4bfb29cf3bdb4e5cb4d5572f3d2b9c89a6c
Omar Ansari <omaransari1@gmail.com>
2017-06-17 01:52:30 -0500

    Add mount_uploader helper for avatar, cover for user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  acts_as_commentable
+  acts_as_votable
+
   mount_uploader :attachment, AvatarUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   acts_as_follower
   acts_as_followable
+  acts_as_voter
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -11,4 +12,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :events
+
+  mount_uploader :avatar, AvatarUploader
+  mount_uploader :cover, AvatarUploader
+
 end
