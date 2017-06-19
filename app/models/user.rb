5ce0e87794454c2c975b6ed8f6944db34c3c50af
Omar Ansari <omaransari1@gmail.com>
2017-06-19 02:41:02 -0500

    Add name entry to user sign up form, add default url host option in config/environment.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
+  validates_presence_of :name
+
   mount_uploader :avatar, AvatarUploader
   mount_uploader :cover, AvatarUploader
 
